e20e2ac5bcf3562add9ab503fe0ceb8f90702355
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-28 22:04:17 -0500

    Update connection.py

--- st_weaviate_connection/connection.py
+++ st_weaviate_connection/connection.py
@@ -2,7 +2,9 @@ from typing import Optional
 
 import pandas as pd
 import weaviate
-from streamlit.connections import BaseConnection
+
+from streamlit.legacy_caching import BaseConnection
+
 from streamlit.runtime.caching import cache_data
 from weaviate.client import Client
 
